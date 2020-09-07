1f80a5dff76e626274bf13327da922efd27b62a8
Yurii-P-Biliai <u.p.bilyay@npu.edu.ua>
2020-09-07 05:45:34 +0300

    Producer-Consumer synchronized

--- src/main/java/ua/edu/npu/thread1/Producer.java
+++ src/main/java/ua/edu/npu/thread1/Producer.java
@@ -13,6 +13,11 @@ public class Producer implements Runnable {
         int i = 0;
 
         while (true) {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             q.put(i++);
         }
     }
